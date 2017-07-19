9ea2c012886f53c9dbe24c1679bf1b7ddea0af50
adrianp <adrian.popa.gh@gmail.com>
2017-07-19 14:24:35 +0300

    IR AC - More comments, turn off jet mode when modifying temperature, fan mode

--- external-scripts/ir-ac-mqtt-agent.py
+++ external-scripts/ir-ac-mqtt-agent.py
@@ -20,6 +20,10 @@ import sys
 # sudo systemctl start ir-ac-mqtt-agent
 
 
+"""
+Parse and load the configuration file to get MQTT credentials
+"""
+
 conf=[]
 with open("/etc/ir-ac-mqtt-agent.yaml", 'r') as stream:
     try:
@@ -35,10 +39,16 @@ mqttTopics = ['ha/lg_ac/power/set', 'ha/lg_ac/jet/set', 'ha/lg_ac/ionizer/set', 
 mqttUser = conf['mqttUser']
 mqttPass = conf['mqttPass']
 
+"""
+Define a state object to preserve an internal AC state so that we can have more complex decisions later
+"""
+
 currentState = { 'power': False, 'jet': False, 'ionizer': False, 'swing': False, 'temperature': 22, 'fan': 'low' }
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
+    """
+    The callback for when the client receives a CONNACK response from the MQTT server.
+    """
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
@@ -48,42 +58,69 @@ def on_connect(client, userdata, flags, rc):
         
         print("Got subscription result for "+topic+":"+str(result))
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+    """
+    The callback for when a PUBLISH message is received from the MQTT server.
+    """
     print("Received command:"+msg.topic+" "+str(msg.payload))
 
-    #listen to requests, process them and call the IR message
-    #set the replies over mqtt
+    """
+    listen to requests, process them and call the IR message
+    set the replies over mqtt
+    """
 
+
+    """
+    Handle AC power on/off
+    """
     if msg.topic == 'ha/lg_ac/power/set':
         if msg.payload == 'ON' or msg.payload == 'OFF':
             processCommand('power', msg.payload)
             if msg.payload == 'OFF':
-                #on power off, disable jet, ionizer and swing
-                for item in ['jet', 'ionizer', 'swing']:
+                #on power off, disable jet, ionizer
+                for item in ['jet', 'ionizer']:
                     currentState[item] = False
                     client.publish('ha/lg_ac/'+item+'/get', 'OFF', 0, False)
+
+    """
+    Handle the pressing of the Jet (or Turbo) button
+    """
     if msg.topic == 'ha/lg_ac/jet/set':
         if currentState['power'] == True:
             if msg.payload == 'ON' or msg.payload == 'OFF':
-                processCommand('jet', msg.payload)
-                if msg.payload == 'ON':
-                    #jet ON means temperature = 18 and fan = high
-                    currentState['temperature'] = 18
-                    client.publish('ha/lg_ac/temperature/get', 18, 0, False)
-                    currentState['fan'] = 'high'
-                    client.publish('ha/lg_ac/fan/get', 'high', 0, False)
+                newstate = True if msg.payload == 'ON' else False
+                if currentState['jet'] != newstate:
+                    #execute the Jet command only if we need to
+                    processCommand('jet', msg.payload)
+                    if msg.payload == 'ON':
+                        #jet ON means temperature = 18 and fan = high
+                        currentState['temperature'] = 18
+                        client.publish('ha/lg_ac/temperature/get', 18, 0, False)
+                        currentState['fan'] = 'high'
+                        client.publish('ha/lg_ac/fan/get', 'high', 0, False)
         #don't set the jet if the AC is off
+
+    """
+    Handle the pressing of the Ionizer button
+    """
     if msg.topic == 'ha/lg_ac/ionizer/set':
         if currentState['power'] == True:
             if msg.payload == 'ON' or msg.payload == 'OFF':
                 processCommand('ionizer', msg.payload)
         #don't set the ionizer if the AC is off
+
+    """
+    Handle the pressing of the Swing button
+    """
     if msg.topic == 'ha/lg_ac/swing/set':
         if currentState['power'] == True:
             if msg.payload == 'ON' or msg.payload == 'OFF':
                 processCommand('swing', msg.payload)
         #don't set the swing if the AC is off
+
+    """
+    Handle the changing of the temperature
+    """
     if msg.topic == 'ha/lg_ac/temperature/set':
         if currentState['power'] == True:
             #The payload should be a temperature
@@ -93,6 +130,14 @@ def on_message(client, userdata, msg):
                 #it avoids an annoying extra beep when setting Jet=ON
                 if temperature != currentState['temperature']:
                     processCommand('temperature', temperature)
+                    #when temperature changes, Jet mode turns off
+                    if currentState['jet']:
+                        currentState['jet'] = False
+                        client.publish('ha/lg_ac/jet/get', 'OFF', 0, False)
+
+    """
+    Handle changing fan speed
+    """
     if msg.topic == 'ha/lg_ac/fan/set':
         if currentState['power'] == True:
             fan = str(msg.payload).lower()
@@ -100,9 +145,16 @@ def on_message(client, userdata, msg):
                 #if the fan is the same as the current state, don't set it
                 if fan != currentState['fan']:
                     processCommand('fan', fan)
+                    #when fan speed changes, Jet mode turns off
+                    if currentState['jet']:
+                        currentState['jet'] = False
+                        client.publish('ha/lg_ac/jet/get', 'OFF', 0, False)
 
 
 def processCommand(item, state):
+    """
+    Call the sendir function, update internal state and publish the state change as an MQTT message
+    """
     print("Setting "+item+" "+str(state))
     success = sendir(item+"-"+str(state).lower())
     if success:
@@ -117,7 +169,7 @@ def processCommand(item, state):
             currentState[item]= True if state == 'ON' else False
         client.publish('ha/lg_ac/'+item+'/get', state, 0, False)
     else:
-        #IR didn't work. Don't change the internal state, but publish it for feedback
+        #IR didn't work. Don't change the internal state, but publish it for feedback (e.g. the slider moves back)
         print("IR injection failed!")
         client.publish('ha/lg_ac/'+item+'/get', state, 0, False)
 
@@ -125,24 +177,35 @@ def processCommand(item, state):
 # Actually send IR codes
 # Returns True if there was no problem reported by lirc, False otherwise
 def sendir(code):
+    """
+    Send IR codes with the help of irsend (lirc). The code names are derived from the item name and lowercase state.
+    E.g.: temperature-22, fan-high, power-on, jet-off.
+    Returns True on succes and False otherwise
+    """
     success = False
+    
+    #due to a bug in lirc/ir module,  we need to restart lirc before/after every code
+    ret = call(['/usr/sbin/service', 'lirc', 'restart'])
+    #sometimes lirc fails to restart if restarted too fast (e.g. on multiple commands)
+    if ret:
+        #try again after a delay
+        time.sleep(5)
+        ret = call(['/usr/sbin/service', 'lirc', 'restart'])
+
     syslog.syslog('Sending IR code '+code)
     ret = call(['/usr/bin/irsend', 'SEND_ONCE', 'lgirplus.conf', code])
+    #ret is the program return code - 0 for success.
     if ret:
         #there was a problem sending the command.
         success = False
     else:
         success = True
 
-    #due to a bug in lirc/ir module,  we need to restart lirc after every code
-    ret = call(['/usr/sbin/service', 'lirc', 'restart'])
-    #sometimes lirc fails to restart if restarted too fast (e.g. on multiple commands)
-    if ret:
-        #try again after a delay
-        time.sleep(3)
-        ret = call(['/usr/sbin/service', 'lirc', 'restart'])
-
     return success
+
+"""
+Initialize the MQTT object and connect to the server, looping forever waiting for messages
+"""
 
 client = mqtt.Client()
 client.on_connect = on_connect
