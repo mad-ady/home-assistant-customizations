fff6931066028274a9483e0b77f2c6eb30a0d8cc
Adrian Popa <adrian.popa.gh@gmail.com>
2017-12-02 10:49:59 +0200

    Custom dashboard skin based on default for small widget sizes (tested with 38x38)

---
+++ appdaemon_skins/defaultsmall/dashboardsmall.js
@@ -0,0 +1,32 @@
+window.onload = function(){
+	console.log("Js onLoad");
+	clearIconAbsolute();
+	h2IconSize();
+}
+
+function clearIconAbsolute(){
+	var sheet = document.styleSheets[0];
+	var rules = sheet.cssRules || sheet.rules;
+	console.log("Extracted CSS rules");
+	for(var i=0; i < rules.length; i++){
+		//console.log("Looking at "+rules[i].selectorText);
+		//do a regex to see if it's 
+		// .widget-javascript-default-.* .icon
+		if(/\.widget-javascript-default-.* \.icon/.test(rules[i].selectorText)){
+			console.log("Found "+rules[i].selectorText);
+			rules[i].style['position'] = "";
+		}
+	} 
+}
+
+function h2IconSize(){
+	var sheet = document.styleSheets[0];
+        var rules = sheet.cssRules || sheet.rules;
+        console.log("Extracted CSS rules");
+        for(var i=0; i < rules.length; i++){
+		if(/h2/.test(rules[i].selectorText)){
+			console.log("Found "+rules[i].selectorText);
+			rules[i].style['font-size'] = '250%';
+		}
+	}
+}
