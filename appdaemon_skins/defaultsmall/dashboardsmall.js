cc1ee686596159fa04c9e2e1c5f1296a2def7a0a
Adrian Popa <adrian.popa.gh@gmail.com>
2017-12-07 16:29:38 +0200

    Add fixes for small script widgets

--- appdaemon_skins/defaultsmall/dashboardsmall.js
+++ appdaemon_skins/defaultsmall/dashboardsmall.js
@@ -12,7 +12,7 @@ function clearIconAbsolute(){
 		//console.log("Looking at "+rules[i].selectorText);
 		//do a regex to see if it's 
 		// .widget-javascript-default-.* .icon
-		if(/\.widget-javascript-default-.* \.icon/.test(rules[i].selectorText)){
+		if(/\.widget-(javascript|baseswitch)-default-.* \.icon/.test(rules[i].selectorText)){
 			console.log("Found "+rules[i].selectorText);
 			rules[i].style['position'] = "";
 		}
