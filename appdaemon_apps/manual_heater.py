053c38c08bd66e3406ce3aae549f5251aa01b2e3
Adrian Popa <adrian.popa.gh@gmail.com>
2018-01-03 11:24:56 +0200

    Fix a bug where this code would turn off a running thermostat on an off period

--- appdaemon_apps/manual_heater.py
+++ appdaemon_apps/manual_heater.py
@@ -2,18 +2,8 @@ import appdaemon.appapi as appapi
 import re
 import datetime
 
-# Turn off climate control on demand, based on input_booleans
-# Manually turn on/off heater based on those input_booleans
-# apps.yaml configuration example:
-# manual_heater:
-#   module: manual_heater
-#   class: ManualHeater
-#   climate: "climate.heater_thermostat"
-#   heater: "switch.heater"
-#   interval_length: 15
-
 class ManualHeater(appapi.AppDaemon):
-    # load data from the configuration section
+    #load data from the configuration section
     interval_length = 15
     climate = "climate.heater_thermostat"
     heater = "switch.heater"
@@ -21,42 +11,39 @@ class ManualHeater(appapi.AppDaemon):
     # called automatically by AppDaemon when starting the App
     def initialize(self):
         self.log("__function__@__line__: Initializing...")
-        # set globals based on the configuration apps.yaml
+        #set globals based on the configuration apps.yaml
         self.interval_length = self.args["interval_length"]
         self.climate = self.args["climate"]
         self.heater = self.args["heater"]
-
-        # register a recurrent timer callback every interval_length minutes (runs every 5th second of the minute
+        
+        #register a recurrent timer callback every interval_length minutes (runs every 5th second of the minute
         self.minutely = self.run_minutely(self.checkManualHeater, datetime.time(0, 0, 5))
-        # register callbacks to all the input_boolean.heater_timer_*_* entities
+        #register callbacks to all the input_boolean.heater_timer_*_* entities
         pattern = re.compile(r"^heater_timer_([0-9]{2})_([0-9]{2})$")
         for item in self.entities.input_boolean:
             result = pattern.search(str(item))
             if result:
-                self.log("__function__@__line__: Looking at input_boolean " + str(item))
-                # register a callback for this one. Send the hour/minute already parsed
-                self.listen_state(self.inputBoolean, "input_boolean." + str(item), hour=result.group(1),
-                                  minute=result.group(2))
+                self.log("__function__@__line__: Looking at input_boolean "+str(item))
+                #register a callback for this one. Send the hour/minute already parsed
+                self.listen_state(self.inputBoolean, "input_boolean."+str(item), hour=result.group(1), minute=result.group(2))
 
     # called when the user toggles a input_boolean switch in order to adjust manual schedule
     def inputBoolean(self, entity, attribute, old, new, kwargs):
-        self.log("__function__@__line__: Hour is " + kwargs["hour"] + ", minute is " + kwargs[
-            "minute"] + ", state is " + new)
-        # get the current time, according to AppDaemon (see TimeTravel in the documentation)
+        self.log("__function__@__line__: Hour is "+kwargs["hour"]+", minute is "+kwargs["minute"]+", state is "+new)
+        #get the current time, according to AppDaemon (see TimeTravel in the documentation)
         currentHour = int(self.time().strftime("%H"))
         currentMinute = int(self.time().strftime("%M"))
-        # see if we are in the same time period as this toggled boolean.
-        # e.g. if the input_boolean for 04:30 was activated, and it's now 04:35 (less than interval_length)
-        # ignore the date part, we're just comparing times
-        booleanTime = datetime.datetime(year=2000, month=1, day=1, hour=int(kwargs["hour"]),
-                                        minute=int(kwargs["minute"]))
+        #see if we are in the same time period as this toggled boolean.
+        #e.g. if the input_boolean for 04:30 was activated, and it's now 04:35 (less than interval_length)
+        #ignore the date part, we're just comparing times
+        booleanTime = datetime.datetime(year=2000, month=1, day=1, hour=int(kwargs["hour"]), minute=int(kwargs["minute"]))
         currentTime = datetime.datetime(year=2000, month=1, day=1, hour=currentHour, minute=currentMinute)
         difference = currentTime - booleanTime
-        # self.log("booleanTime = " + str(booleanTime) + ", currentTime = " + str(currentTime) + ", difference is "+str(difference.total_seconds()))
-        if difference.total_seconds() >= 0 and difference.total_seconds() <= self.interval_length * 60:
-            # somebody toggled this interval and we're in it!
+        #self.log("booleanTime = " + str(booleanTime) + ", currentTime = " + str(currentTime) + ", difference is "+str(difference.total_seconds()))
+        if difference.total_seconds() >= 0 and difference.total_seconds() <= self.interval_length*60:
+            #somebody toggled this interval and we're in it!
             self.log("__function__@__line__: Interval was toggled and we're currently in it.")
-            self.controlHeater(new)
+            self.controlHeater(new, old)
         else:
             self.log("__function__@__line__: Not my interval. Ignoring it for now")
 
@@ -64,32 +51,41 @@ class ManualHeater(appapi.AppDaemon):
     def checkManualHeater(self, kwargs):
         currentMinute = int(self.time().strftime("%M"))
         if currentMinute % self.interval_length == 0:
-            # we're on an interval_length boundry
+            #we're on an interval_length boundry
             self.log("__function__@__line__: Checking if we need to touch climate")
             currentHour = int(self.time().strftime("%H"))
-            # get the state of the input_boolean
+            #get the state of the input_boolean
             booleanName = "heater_timer_%02d_%02d" % (currentHour, currentMinute)
-            currentState = eval("self.entities.input_boolean." + booleanName + ".state")
-            self.log("__function__@__line__: %s is %s" % (booleanName, currentState))
-            self.controlHeater(currentState)
+            currentState = eval("self.entities.input_boolean."+booleanName+".state")
+            self.log("__function__@__line__: %s is %s"%(booleanName, currentState))
+
+            #get data for the previous interval as well. Get the current time HH:MM and deduct an interval_length and convert it back to time
+            #previousTime = self.time() - self.interval_length * 60
+            #TODO
+
+            self.controlHeater(currentState, None)
 
     # take over the heater controls
-    def controlHeater(self, newstate):
+    def controlHeater(self, newstate, oldstate):
+        self.log("__function__@__line__: newstate: %s, oldstate: %s" % (newstate, oldstate))
+        thermostat_threshold = eval("self.entities."+self.climate+".attributes.temperature")
+        thermostat_current = eval("self.entities."+self.climate+".attributes.current_temperature")
+        self.log("__function__@__line__: thermostat threshold: %f, thermostat current temperature: %f" % (thermostat_threshold, thermostat_current))
         if newstate == "on":
-            # set climate off
+            #set climate off
             if eval("self.entities." + self.climate + ".attributes.operation_mode") != "off":
                 self.log("__function__@__line__: Turning off climate control")
-                self.call_service("climate/set_operation_mode", entity_id=self.climate, operation_mode="off")
-            # set heater on
+                self.call_service("climate/set_operation_mode", entity_id = self.climate, operation_mode = "off" )
+            #set heater on
             if eval("self.entities." + self.heater + ".state") != "on":
                 self.log("__function__@__line__: Turning on heater switch")
-                self.call_service("switch/turn_on", entity_id=self.heater)
+                self.call_service("homeassistant/turn_on", entity_id = self.heater)
         else:
-            # set climate on
+            #set climate on
             if eval("self.entities." + self.climate + ".attributes.operation_mode") != "auto":
                 self.log("__function__@__line__: Turning on climate control")
                 self.call_service("climate/set_operation_mode", entity_id=self.climate, operation_mode="auto")
-            # set heater off
-            if eval("self.entities." + self.heater + ".state") != "off":
+            #set heater off
+            if eval("self.entities." + self.heater + ".state") != "off" and thermostat_current >= thermostat_threshold:
                 self.log("__function__@__line__: Turning off heater switch")
-                self.call_service("switch/turn_off", entity_id=self.heater)
+                self.call_service("homeassistant/turn_off", entity_id=self.heater)
