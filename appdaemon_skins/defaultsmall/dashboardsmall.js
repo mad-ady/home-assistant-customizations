2f48acce653800b11972656120f9f2ae3aff2049
Adrian Popa <adrian.popa.gh@gmail.com>
2018-06-14 13:08:03 +0300

    Fixed for appdaemon 3.x compatibility

--- appdaemon_skins/defaultsmall/dashboardsmall.js
+++ appdaemon_skins/defaultsmall/dashboardsmall.js
@@ -16,7 +16,7 @@ function clearIconAbsolute(){
 		//console.log("Looking at "+rules[i].selectorText);
 		//do a regex to see if it's 
 		// .widget-javascript-default-.* .icon
-		if(/\.widget-(javascript|baseswitch)-default-.* \.icon/.test(rules[i].selectorText)){
+		if(/\.widget-(javascript|baseswitch|basejavascript)-default-.* \.icon/.test(rules[i].selectorText)){
 			console.log("Found "+rules[i].selectorText);
 			rules[i].style['position'] = "";
 		}
