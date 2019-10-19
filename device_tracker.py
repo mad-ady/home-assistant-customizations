4e6840087a9dbea60969011d2c7f0f6f07324cd2
CB <terry.chad@gmail.com>
2019-10-19 12:39:19 -0700

    Add GPS retrieval for AP's and Clients
    Transitioning to non-legacy Device class

--- device_tracker.py
+++ device_tracker.py
@@ -54,6 +54,7 @@ def get_scanner(hass, config):
     return KismetDeviceScanner(hass, config[DOMAIN])
 
 
+DeviceGPS = namedtuple('Device', ['mac', 'last_ssid', 'gps', 'last_update'])
 Device = namedtuple('Device', ['mac', 'last_ssid', 'last_update'])
 
 class KismetDeviceScanner(DeviceScanner):
@@ -135,17 +136,27 @@ class KismetDeviceScanner(DeviceScanner):
         _LOGGER.debug("Preparing kismet query...")
         last_results = []
         #prepare the query
-        parameters = {}
-        parameters['regex'] = []
-        for ssid in self.ssids:
-            _LOGGER.debug("Adding SSID "+ssid+"...")
-            parameters['regex'].append(['dot11.device/dot11.device.last_beaconed_ssid', str(ssid)])
 
+        # two different records for client/ssid
+        ssid_gps_prefix = "dot11.device/dot11.device.last_beaconed_ssid_record/dot11.advertisedssid.location"
+        client_gps_prefix = "dot11.device/dot11.device.last_probed_ssid_record/dot11.probedssid.location"
+        
+        parameters = {"regex": [], "fields": ["kismet.device.base.name", "kismet.device.base.macaddr"] }
+
+        if len(self.ssids):
+            parameters["fields"].append(ssid_gps_prefix)
+        
+        if len(self.clients):
+            parameters["fields"].append(client_gps_prefix)
+
+        for ssid in self.ssids:
+            _LOGGER.debug("Adding SSID " + ssid + "...")
+            parameters["regex"].append(["kismet.device.base.name", str(ssid)])
+        
         for client in self.clients:
             _LOGGER.debug("Adding client " + client + "...")
-            parameters['regex'].append(['kismet.device.base.macaddr', str(client).upper()])
-
-        parameters['fields'] = ('kismet.device.base.macaddr', 'kismet.device.base.name')
+            parameters["regex"].append(["kismet.device.base.macaddr", str(client).upper()])
+        
 
         #payload = "json="+urllib.parse.quote_plus(json.dumps(parameters))
         payload = "json="+json.dumps(parameters)
@@ -156,18 +167,35 @@ class KismetDeviceScanner(DeviceScanner):
                 headers={ 'Content-Type': 'application/x-www-form-urlencoded'},
                 data=payload,
                 auth=(self.user, self.password))
-
+            
             now = dt_util.now()
+            
             if r.ok:
                 if r.json():
                     # we got a valid reply. Should look like this:
-                    #[{'kismet.device.base.macaddr': 'AA:BB:CC:DD:EE:FF', 'kismet.device.base.name': 'My Device Name'}]
+                    #[{'kismet.device.base.macaddr': 'AA:BB:CC:DD:EE:FF', 'kismet.device.base.name': 'My Device Name',
+                    #  'dot11.[probed/advertised]ssid.location': { ... } }]
 
+                    _LOGGER.info(r.json())
                     for pair in r.json():
                         _LOGGER.debug("Found device "+str(pair['kismet.device.base.macaddr']))
-                        last_results.append(Device(pair['kismet.device.base.macaddr'].upper(), pair['kismet.device.base.name'], now))
+
+                        if "dot11.probedssid.location" in pair and pair["dot11.probedssid.location"] != 0:
+                            location = pair["dot11.probedssid.location"]
+                        
+                        elif "dot11.advertisedssid.location" in pair and pair["dot11.advertisedssid.location"] != 0:
+                            location = pair["dot11.advertisedssid.location"]
+                         
+                        if location and "kismet.common.location.loc_valid" in location and location["kismet.common.location.loc_valid"] == 1:
+                            # instead of delving further into the structure, we use the integer coordinates
+                            lat = location["kismet.common.location.avg_lat"] * .000001
+                            lon = location["kismet.common.location.avg_lon"] * .000001
+                            last_results.append(DeviceGPS(pair["kismet.device.base.macaddr"].upper(), pair["kismet.device.base.name"], (lat,lon), now))
+                        else:
+                            _LOGGER.debug("Couldn't find GPS Coordinates in result..")
+                            last_results.append(Device(pair["kismet.device.base.macaddr"].upper(), pair["kismet.device.base.name"], now))
                 else:
-                    _LOGGER.error("Got an error in the kismet reply: "+r.text)
+                    _LOGGER.error(f"Got an error in the kismet reply: {r.text}")
                     pass
             else:
                 _LOGGER.error(f"Got an error in the kismet query. Error code {r.status_code}, reply text {r.text}")
@@ -177,5 +205,5 @@ class KismetDeviceScanner(DeviceScanner):
 
         self.last_results = last_results
 
-        _LOGGER.info("Kismet scan finished")
+        _LOGGER.debug("Kismet scan finished")
         return True
